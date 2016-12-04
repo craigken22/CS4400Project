abcb60847e78862d59aac05cd21e76263060f33f
Kenneth Craig <craigken22@gmail.com>
2016-12-04 17:59:30 -0500

    Queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -12,27 +12,57 @@ SELECT	u.username,
 	FROM STD_USERS u
 		JOIN LKP_MAJORS m ON u.major = m.major
 	WHERE u.username = '' AND u.password = ''
-	
+
 SELECT major
      FROM LKP_MAJORS
      WHERE department = dept
-	 
+
 SELECT category
      FROM LKP_CATEGORIES;
-	 
+
 SELECT year
      FROM LKP_YEARS;
-	 
+
 SELECT designation
      FROM LKP_DESIGNATIONS;
-	 
+
 SELECT username,
          project_name,
          status
      FROM BRG_APPLICATIONS;
-	 
+
 SELECT username,
          project_name,
          status
      FROM BRG_APPLICATIONS;
-	 
+
+INSERT INTO STD_USERS
+				(username, user_type, password, major, year, email_address)
+		VALUES
+				('', '', '', '', '', '')
+
+INSERT INTO STD_PROJECTS
+				(project_name, advisor, description, designation, major_restriction,
+						year_restriction, dept_restriction, est_num_students)
+				VALUES
+						('', '', '', '', '', '', '', '')
+
+INSERT INTO STD_COURSES
+				(course_num, course_name, instructor, designation, est_num_students)
+				VALUES
+						('', '', '', '', '')
+
+--Check if the email_address is already in the database
+SELECT email_address
+		FROM STD_USERS u
+		WHERE u.email_address = ''
+
+-- Update logged in users year
+UPDATE STD_USERS
+		SET year = ''
+		WHERE username = ''
+
+-- Update logged in users major
+UPDATE STD_USERS
+		SET major = ''
+		WHERE username = ''
